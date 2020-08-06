57e3ad67c2d0bc3057ffff254045c9602231d0b3
Artur Shapovalov <shapovalov.artur@gmail.com>
2020-08-06 09:57:49 +0300

    Add files via upload

--- esub_loader.py
+++ esub_loader.py
@@ -3,20 +3,24 @@ provide import of report lists from xml e-submission file
 
 """
 import xml.etree.cElementTree as ET
+import ntpath
 
 
 def get_rl_id(xml_file):
     """
-    :return: return rl_id value from xml filename
+    return rl_id value from xml filename
+    'xml_file' - path to submission xml file
     """
     return xml_file[xml_file.rfind('RL'):(xml_file.rfind('RL') + 4)]
 
 
 def get_p_id(xml_file):
     """
-    :return: return p_id value from xml filename
-    find '_'+periodicity key+'_' in name
+    return p_id value from xml filename
+
+    'xml_file' - path to submission xml file
     """
+    #find '_'+periodicity key+'_' in name
     for element_key in ('A, H, Q, M'):
         if '_' + element_key + '_' in xml_file:
             p_id = element_key
@@ -24,13 +28,13 @@ def get_p_id(xml_file):
     # function gets xml submission file and return a dictionary of nillable reports:
 
 
-def get_nil(xml_file, flag='false'):
+def get_nil(xml_file, flag='false', return_list=True):
     """
-    returns a dictionary of reports as a keys and submit option as value of 'Nil_Form' element
-    'xml_file' - submission xml file
-    'false' (default) - return report elements with 'false' subelement
-    'all' - return all report elements
-    'true' - return report elements with 'true' subelements
+    returns a list of reports of 'Nil_Form' element
+    'xml_file' - path to submission xml file
+    flag - 'false' (default) - return report elements with 'false' subelement
+           'all' - return all report elements
+           'true' - return report elements with 'true' subelements
     """
     tree = ET.ElementTree(file=xml_file)
     root = tree.getroot()
@@ -42,15 +46,16 @@ def get_nil(xml_file, flag='false'):
             key_element = element.tag[element.tag.find('_', element.tag.find('_')+1,-1)+1:]
             #create dictionary with all elements
             nil_dict.update({key_element: sub_element.text})
-    # return dictionary with selected 'flag'
+    # create and return list with selected 'flag'
     if flag == 'all':
-        return nil_dict
+        nil_list = [i for i in nil_dict.keys()]
+        return nil_list
     elif flag == 'false':
-        false_nil_dict = {key:value for (key,value) in nil_dict.items() if value != 'true'}
-        return false_nil_dict
+        nil_list = [key for key in nil_dict.keys() if nil_dict[key] == 'false']
+        return nil_list
     elif flag == 'true':
-        true_nil_dict = {key:value for (key,value) in nil_dict.items() if value == 'true'}
-        return true_nil_dict
+        nil_list = [key for key in nil_dict.keys() if nil_dict[key] == 'true']
+        return nil_list
 
 
 def get_report_list(xml_file):
@@ -64,33 +69,29 @@ def get_report_list(xml_file):
     return [i.tag for i in root.findall('.//A1_x0020_Repeat_x0020_Group/*[@type="group"]') if i.tag != 'Nil_Form']
 
 
-def create_report_list(xml_file):
+def get_total_list(xml_file):
     """
-    returns a dictionary of
-    'xml_file' - submission xml file
-    {'file name' : 'MAS610_RL08_H_20190331.xml',
-    'RL_ID':'RL08',
-    'P_ID' :'H',
-    'Nil_Form': (),
-    'Report List': ()
-    }
-
+    'xml_file' - path to submission xml file
+    returns a tuple with elements:
+    0 - xml file name
+    1 - report level
+    2 - periodicity level
+    3 - report list from 'Nil_Form' element,
+    4 - report list from submission xml file
     """
+    # extract filename from path
+    xml_file_name = ntpath.basename(xml_file)
 
-    final_dict = {
-                    'File name': xml_file,
-                    'RL_ID': get_rl_id(xml_file),
-                    'P_ID': get_p_id(xml_file),
-                    'Nil_Form': get_nil(xml_file),
-                    'Report List': get_report_list(xml_file)
-                    }
-    return final_dict
+    total_list = (xml_file_name, get_rl_id(xml_file), get_p_id(xml_file),
+                get_nil(xml_file, 'false'), get_report_list(xml_file))
+
+    return total_list
 
 
 if __name__ == '__main__':
-    # print(get_rl_id('d:\\PROG\\MAS SUB_TESTER\\test_data\\MAS1003 RL11_Q_20190331.xml'))
-    # print(get_p_id('d:\\PROG\\MAS SUB_TESTER\\test_data\\MAS1003 RL11_Q_20190331.xml'))
-    print(create_report_list('data/MAS610_RL08_A_20190331.xml'))
-    print(create_report_list('data/MAS610_RL08_A_20190331.xml'))
-    print(create_report_list('data/MAS610_RL08_A_20190331.xml'))
-    print(create_report_list.__doc__)
+    # # print(get_rl_id('d:\\PROG\\MAS SUB_TESTER\\test_data\\MAS1003 RL11_Q_20190331.xml'))
+    # # print(get_p_id('d:\\PROG\\MAS SUB_TESTER\\test_data\\MAS1003 RL11_Q_20190331.xml'))
+    # # print(get_nil('data/MAS610_RL08_A_20190331.xml', 'all'))
+    # # print(get_nil('data/MAS610_RL08_A_20190331.xml', 'true'))
+    # # print(get_nil('data/MAS610_RL08_A_20190331.xml', 'false'))
+    # print(get_total_list('data/MAS610_RL08_A_20190331.xml'))
